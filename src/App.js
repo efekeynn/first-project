7cf9906775e0494bb85c29d2e3fd19b7a00aed3b
Efekan <efekanbolat005@gmail.com>
2024-12-16 13:39:26 +0300

    H1,H2,H3 EKLENIP COLOR ATILDI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './App.css';
 
 function App() {
   return (
     <div>
-      efe
+      <h1>EFEKAN</h1>
+      <h2>BARIŞ</h2>
+      <h3>BOLAT</h3>
+
     </div>
   )
 }
